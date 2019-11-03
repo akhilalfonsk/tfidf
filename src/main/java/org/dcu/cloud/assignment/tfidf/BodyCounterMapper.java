0052168d1eca5cd450c9a091039fd32355d90e73
Akhil <akhil.alfons.k@gmail.com>
2019-11-03 22:11:33 +0000

    Modifications

---
+++ src/main/java/org/dcu/cloud/assignment/tfidf/BodyCounterMapper.java
@@ -0,0 +1,21 @@
+package org.dcu.cloud.assignment.tfidf;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import java.io.IOException;
+
+public class BodyCounterMapper extends Mapper<LongWritable, Text, Text, IntWritable>{
+        public void map(LongWritable key, Text value, Context con) throws IOException, InterruptedException {
+            String line = value.toString();
+            String id = line.substring(0,line.indexOf(","));
+            line=line.substring(line.indexOf(",")+1);
+            String user = line.substring(0,line.indexOf(","));
+            String body = line.substring(line.indexOf(",")+1);
+            Text outputKey = new Text(new Text(user));
+            IntWritable outputValue = new IntWritable(1);
+            con.write(outputKey, outputValue);
+        }
+}
--- src/main/java/org/dcu/cloud/assignment/tfidf/TFIDFDriver.java
+++ src/main/java/org/dcu/cloud/assignment/tfidf/TFIDFDriver.java
@@ -4,6 +4,7 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
@@ -17,28 +18,54 @@ import org.apache.hadoop.util.ToolRunner;
 public class TFIDFDriver extends Configured implements Tool {
     private static final Log log = LogFactory.getLog(TFIDFDriver.class);
 
+    private static final String documentCollectionInput="data/touserpost.csv";
+    private static final String postCountPath="data/output/postcount";
+    private static final String postWordCountPerDoc="data/output/wordcountperdoc";
+    private static final String postWordFrequencyAcrossDoc="data/output/wordfrequency";
+    private static final String topUserTFIDF="data/output/tfidf";
+
+
     public int run(String[] args) throws Exception {
-        Configuration c = new Configuration();
-        String[] files = new GenericOptionsParser(c, args).getRemainingArgs();
-        Path input = new Path(files[0]);
-        Path output = new Path(files[1]);
-        Job j = new Job(c, "TFIDF");
+        Configuration conf = new Configuration();
 
-        j.setJarByClass(TFIDFDriver.class);
-        j.setMapperClass(BodyToTokenMapper.class);
-        j.setReducerClass(TokenToFrequencyReducer.class);
+        Job bodyCounterJob = Job.getInstance(conf, "BodyCounterJob");
+        bodyCounterJob.setJarByClass(TFIDFDriver.class);
+        bodyCounterJob.setMapperClass(BodyToTokenMapper.class);
+        bodyCounterJob.setReducerClass(BodyCounterReducer.class);
+        bodyCounterJob.setOutputKeyClass(Text.class);
+        bodyCounterJob.setOutputValueClass(IntWritable.class);
+        bodyCounterJob.setNumReduceTasks(1);
+        FileInputFormat.addInputPath(bodyCounterJob, new Path(documentCollectionInput));
+        FileOutputFormat.setOutputPath(bodyCounterJob, new Path(postCountPath));
 
-        j.setOutputKeyClass(Text.class);
-        j.setOutputValueClass(IntWritable.class);
+        FileSystem hdfs = FileSystem.get(conf);
+        if (hdfs.exists(new Path(postCountPath)))
+            hdfs.delete(new Path(postCountPath), true);
 
-        FileInputFormat.addInputPath(j, input);
-        FileOutputFormat.setOutputPath(j, output);
-
-        return(j.waitForCompletion(true) ? 0 : 1);
+        return(bodyCounterJob.waitForCompletion(true) ? 0 : 1);
     }
 
     public static void main(String[] args) throws Exception{
         int exitCode = ToolRunner.run(new TFIDFDriver(), args);
         System.exit(exitCode);
+    }
+
+    public int wordFrequencyJob() throws Exception{
+        Configuration conf = new Configuration();
+        Job wordFrequencyCounterJob = Job.getInstance(conf, "WordFrequencyCounterJob");
+        wordFrequencyCounterJob.setJarByClass(TFIDFDriver.class);
+        wordFrequencyCounterJob.setMapperClass(BodyToTokenMapper.class);
+        wordFrequencyCounterJob.setReducerClass(TokenToFrequencyReducer.class);
+        wordFrequencyCounterJob.setOutputKeyClass(Text.class);
+        wordFrequencyCounterJob.setOutputValueClass(IntWritable.class);
+        wordFrequencyCounterJob.setNumReduceTasks(1);
+        FileInputFormat.addInputPath(wordFrequencyCounterJob, new Path(documentCollectionInput));
+        FileOutputFormat.setOutputPath(wordFrequencyCounterJob, new Path(postWordCountPerDoc));
+
+        FileSystem hdfs = FileSystem.get(conf);
+        if (hdfs.exists(new Path(postWordCountPerDoc)))
+            hdfs.delete(new Path(postWordCountPerDoc), true);
+
+        return(wordFrequencyCounterJob.waitForCompletion(true) ? 0 : 1);
     }
 }
